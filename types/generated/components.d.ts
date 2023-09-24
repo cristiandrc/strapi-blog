2600b1fd5e8a836b13c5d3eb59a6429b357a6ead
Cristian Rojas <crisdarroca@hotmail.com>
2023-09-23 22:43:31 -0500

    feat: create article author and category

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,19 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface SectionsRichText extends Schema.Component {
+  collectionName: 'components_sections_rich_texts';
+  info: {
+    displayName: 'Rich text';
+  };
+  attributes: {
+    content: Attribute.RichText;
+  };
+}
+
 declare module '@strapi/strapi' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'sections.rich-text': SectionsRichText;
+    }
+  }
 }
